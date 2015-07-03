117d580ac87d2cfc922b1de228e5a683c7a08d9e
wangyingying1234521 <779192557@qq.com>
2015-07-03 17:03:54 +0800

    王莹莹

--- SRSOO.WebSite/App_Code/WebBasePage.cs
+++ SRSOO.WebSite/App_Code/WebBasePage.cs
@@ -5,13 +5,22 @@ using System.Text;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public class WebBasePage: System.Web.UI.Page
+public class WebBasePage : System.Web.UI.Page
 {
     public User CurrentUser
     {
         get
         {
-            return Session["CurrentUser"] as User;
+            if (Session["CurrentUser"] == null)
+            {
+                //转到登录界面
+                return null;
+            }
+            else
+            {
+                return Session["CurrentUser"] as User;
+            }
+
         }
     }
 }
